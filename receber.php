e10db44fe4bb5bfe6c1f029ccc4fab53ca72fbae
Luis Felipe  Santos <50251368+LuisFelipeSantosdosSantos@users.noreply.github.com>
2019-10-04 20:44:33 -0300

    depois do intervalo
    a

---
+++ receber.php
@@ -0,0 +1,34 @@
+<?php
+#back-end
+if(isset($_GET['nome'])){
+    echo "\n<pre>\n";
+    print_r($_GET); #Array
+    echo "\n</pre>\n";
+
+    print("\n<br><strong>Nome: </strong>");
+    print("$_GET[nome]");
+
+    print("\n<br><strong>E-mail:</strong>");
+    print("$_GET[email]");
+
+    print("\n<br><strong>Senha:</strong>");
+    print("$_GET[senha]");
+
+    print("\n<br><strong>Data de Nascimento:</strong>");
+    print("$_GET[data_de_nascimento]"); 
+}
+//VERIFICA SE O METODO POST ESTA ENVIANDO DADOS
+
+if(isset($_POST['nome'])){
+    print("\n<br><strong>Nome: </strong>");
+    print("$_POST[nome]");
+
+    print("\n<br><strong>E-mail:</strong>");
+    print("$_POST[email]");
+
+    print("\n<br><strong>Senha:</strong>");
+    print("$_POST[senha]");
+
+    print("\n<br><strong>Data de Nascimento:</strong>");
+    print("$_POST[data_de_nascimento]"); 
+}
