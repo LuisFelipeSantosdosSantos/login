821f3d5ba7b66a71ec58a70fd28c488e7371b5f1
Luis Felipe  Santos <50251368+LuisFelipeSantosdosSantos@users.noreply.github.com>
2019-10-04 21:16:29 -0300

    conclusao da aula 05
    data 04/10/2019

--- receber.php
+++ receber.php
@@ -1,6 +1,7 @@
 <?php
 #back-end
 if(isset($_GET['nome'])){
+    echo "\n<h1>Envio de dados metodo <em>GET<em></h1>";
     echo "\n<pre>\n";
     print_r($_GET); #Array
     echo "\n</pre>\n";
@@ -20,6 +21,11 @@ if(isset($_GET['nome'])){
 //VERIFICA SE O METODO POST ESTA ENVIANDO DADOS
 
 if(isset($_POST['nome'])){
+    echo"\n<h1>Envio de dados metodo <em>POST<em></h1>";
+    echo "\n<pre>\n";
+    print_r($_POST);#Array
+    echo "\n</pre>\n";
+
     print("\n<br><strong>Nome: </strong>");
     print("$_POST[nome]");
 
