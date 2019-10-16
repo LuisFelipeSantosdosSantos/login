3d5e6b8f2b76cb154bb3766f834b27883a864d42
Luis Felipe Santos <LuisFelipeSantosdosSantos>
2019-10-15 21:20:00 -0300

    MEIO DA AULA 6

--- recebe_dados.php
+++ recebe_dados.php
@@ -7,15 +7,46 @@ function verificar_entrada($entrada){
     $saida = htmlspecialchars($entrada);
     $saida = stripslashes($saida);
     $saida = trim($saida);
+    return $saida; //retorna a saida limpa
 }
 //teste se existe a ação
 if (isset($_POST['action'])) {
     if ($_POST['action'] == 'cadastro') {
         //teste se ação é igual a cadastro
-        echo "\n<p>cadastro</p>";
-        echo "\n<pre>";
-        print_r($_POST);
-        echo "\n</pre>";
+    
+        #echo "\n<p>cadastro</p>";
+        #echo "\n<pre>";
+        #print_r($_POST);
+        #echo "\n</pre>";
+        //pegando dados do formulario
+        $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+        $nomeDoUsuario = verificar_entrada($_POST['nomeU=DoUsuario']);
+        $emailUsuario = verificar_entrada($_POST['emailUsuario']);
+        $senhaDoUsuario = verificar_entrada($_POST['senhaDoUsuario']);
+        $senhaUsuarioConfirmar = verificar_entrada($_POST['senhaUsuarioConfirmar']);
+        $dataCriado = date("Y-m-d"); // data atual no formato banco de dados
+
+        //codificando as senhas
+        $senhaCodificada = sha1($senhaDoUsuario);
+        $senhaConfirmarCod = sha1($senhaUsuarioConfirmar);
+        //teste de captura de dados
+        // echo "<p>Nome completo: $nomeCompleto </p>";
+        // echo "<p>Nome de Usuario: $nomeDoUsuario </p>";
+        // echo "<p>E-mail: $emailUsuario </p>";
+        // echo "<p>Senha codificada: $senhaCodificada</p>";
+        // echo "<p>Data de criação: $dataCriado</p>";
+        if($senhaCodificada != $senhaConfirmarCod){
+            echo "<p class= 'text-danger'>Senha não conferem.</p>";
+            exit();
+        }else{
+            //as senhas conferem, verificar se o usuario ja
+            //existe no banco de dados
+            $sql = $connect ->prepare("SELECT nomeDoUsuario, emailUsuario FROM usuario WHERE nomeDoUsuario = ? OR emailUsuario = ?");
+            $sql->bind_param("ss", $nomeDoUsuario, $emailUsuario);
+            $sql->execute();
+            $resultado = $sql->get_result();
+            $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        }
 
     } else if ($_POST['action'] == 'login') {
         //senão, teste se ação é login
