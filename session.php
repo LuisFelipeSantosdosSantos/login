74a2d25d3812fc95ebcae7525d4b27249f86fecc
Luis Felipe Santos <LuisFelipeSantosdosSantos>
2019-10-23 00:07:24 -0200

    continuação 708

--- session.php
+++ session.php
@@ -4,6 +4,18 @@ require_once "configBD.php";
 
 if (isset($_SESSION['nomeDoUsuario'])) {
     //logado
+    $usuario = $_SESSION['nomeDoUsuario'];
+    $sql = $connect->prepare("SELECT * FROM usuario 
+    WHERE nomeDoUsuario = ?");
+    $sql->bind_param("s",$usuario);
+    $sql->execute();
+    $resultado =  $sql->get_result();
+    $linha = $resultado->fetch_array("MYSQL_ASSOC");
+
+    $nomeDoUsuario = $linha ['nomeDoUsuario'];
+    $nomeCompleto = $linha ['nomeCompleto'];
+    $emailUsuario = $linha ['emailUsuario'];
+    $dataCriado = $linha ['dataCriado'];
 
 } else {
     //Senão estiver logado, rediricionar para index
